14a480d4145fc348916c872f403587480ca31f2c
Caio <caioymc@live.com>
2020-02-10 11:17:49 +0000

    Updates

---
+++ precache-manifest.e89d0d18d53cfcd3f2743ffaea38794e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc43a076e468749e40a427df34b2639",
+    "url": "/scan-challenge/index.html"
+  },
+  {
+    "revision": "ffe466f59dd0781c4dc0",
+    "url": "/scan-challenge/static/css/main.b374cd29.chunk.css"
+  },
+  {
+    "revision": "56c491889d757d6ea46b",
+    "url": "/scan-challenge/static/js/2.23d66dfd.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/scan-challenge/static/js/2.23d66dfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe466f59dd0781c4dc0",
+    "url": "/scan-challenge/static/js/main.58a56973.chunk.js"
+  },
+  {
+    "revision": "83d9a498775bb2ed2851",
+    "url": "/scan-challenge/static/js/runtime-main.e0244910.js"
+  }
+]);
